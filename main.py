158b6c9f272685b41646c6d8efaad84291e3ba50
dhruv <dhruv@gitignore.com>
2025-12-28 18:24:56 -0600

    Working with a local gui option

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-import json
-from flask import Flask, render_template, redirect, url_for
-import threading
-from reader import readfile
+from reader import readfile as rf
+from tkinter import Tk, ttk
 
-
-# Really shit way of doing it, collects the top/new/best posts and puts them to the data.json
 def getposts():
     #Add more urls to increase the number of posts found, maybe add hot and top for higher quality
     urls = ["https://reddit.com/r/TwoSentenceHorror/new/"]
     for i, url in enumerate(urls):
         print(f"Fetching {i} url(s) from {url}")
-        readfile(url)
+        rf(url)
+def printname():
+    print("Hello World")
+def drawimage():
+    root = Tk() 
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+    ttk.Button(frm, text="hello", command=printname).grid(column=1, row=1)
+    root.mainloop()
 
-getposts()
-app = Flask(__name__)
-
-with open("data.json") as f:
-    DATA = json.load(f)
-    KEYS = list(DATA.keys())
-
-current_index = 0
-
-@app.route("/")
-def index():
-    global current_index
-    key = KEYS[current_index]
-    return render_template("index.html", key=key)
-
-@app.route("/reveal")
-def reveal():
-    global current_index
-    key = KEYS[current_index]
-    value = DATA[key]
-    return render_template("solution.html", key=key, value=value)
-
-@app.route("/next")
-def next_key():
-    global current_index
-    current_index = (current_index + 1) % len(KEYS)
-    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    drawimage()
